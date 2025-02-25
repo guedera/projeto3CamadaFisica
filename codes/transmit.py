ee5d283280fac7e4774972939902e32e5dff11b3
guedera <guilhermegg5@al.insper.edu.br>
2025-02-25 19:40:54 -0300

    certo separa e datagrama

--- codes/transmit.py
+++ codes/transmit.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 from autolimpa import clear_terminal
 from separa import separa
+from datagramas import datagrama
 
 serialName = "/dev/ttyACM1"
 
@@ -13,7 +14,7 @@ def main():
         com1.enable()
 
         print("Enviando o byte de sacrifício")
-        com1.sendData(b'0')
+        com1.sendData(b'00')
         print("Byte de sacrifício enviado!\n")
         time.sleep(0.2)
 
@@ -24,10 +25,17 @@ def main():
         bytes_imagem = open(imageR, 'rb').read()
 
         bytes_partes = separa(bytes_imagem)
-        
-        #aqui dividiremos a imagem em pacotes!!
 
-        imagem = bytearray(b'1') #exemplo, aqui vai o pacote!!!
+        for i in range(len(bytes_partes)):
+            data = datagrama(bytes_partes[i],i,1,0,0)
+            txBuffer = data
+            com1.sendData(txBuffer)
+            print("Pacote {} enviado!".format(i))
+            time.sleep(0.5)
+            #agora tenho que confirmar que o server recebeu certo
+            
+
+
 
         com1.sendData(np.asarray(txBuffer))
         
